73738eebeaade79bf86d493387ead5a4569f598c
Smak2025 <smak-80+2025@yandex.ru>
2026-03-13 09:24:00 +0300

    Стасики

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,34 @@
+List<Person> createPersons(String[] lastname, String[] firstname){
+    List<Person> lst = new ArrayList<>();
+    for (int i = 0; i < lastname.length; i++) {
+        lst.add(new Person(lastname[i], firstname[i]));
+        System.out.println(lst.getLast());
+    }
+    return lst;
+}
+
+void showAll(List<Person> persons){
+    for (Person person : persons) {
+        System.out.println(person);
+    }
+}
+
 void main() {
-  $END$
+    System.out.println(Person.getPersonCount());
+    var persons = createPersons(
+            new String[]{"Иванов", "Петров", "Макарова"},
+            new String[]{"Иван", "Петр", "Анна"}
+    );
+    //....
+    System.out.println("==============================");
+    showAll(persons);
+    System.out.println(Person.getPersonCount());
+    System.out.println("==============================");
+    persons.add(persons.get(1).copy());
+    System.out.println(persons.getLast());
+
+    System.out.println("==============================");
+    showAll(persons);
+    System.out.println(Person.getPersonCount());
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Person {
+    private int id;
+    private static int personCount;
+    private String lastname;
+    private String firstname;
+
+    public Person(String lastname, String firstname){
+        id = ++personCount;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Person copy(){
+        return new Person(lastname, firstname);
+    }
+
+    @Override
+    public String toString() {
+        return id+"/"+personCount+". "+lastname + " " + firstname;
+    }
+
+    public static int getPersonCount(){
+        return personCount;
+    }
+
+    public int getId(){
+        return this.id;
+    }
 }
